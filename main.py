9cd628f4a4985fae68f853c1f9d6b1dfb0077a79
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2023-09-17 15:31:30 +0530

    feat: add database local session method

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ from functools import lru_cache
 
 # import config file
 from . import config
+#import db models
+from .models import users_db
+# import db utils
+from .database.db import SessionLocal, engine
+
+# create tables
+users_db.Base.metadata.create_all(bind=engine)
+
 
 # fastapi app
 app = FastAPI()
+
+# Database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # environment variables
 @lru_cache()
